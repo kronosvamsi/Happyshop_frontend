1b0ed9610b75eedb723d4bd7089f7116c2dd2c22
Kronosvamsi <beingaliver@gmail.com>
2026-03-18 12:50:50 +0530

    Add tailwind css and config

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 import Products from "./components/Products";
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to happyshop_Ui </h1>
-      <Products />
-      {/* <Login /> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
